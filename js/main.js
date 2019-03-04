653f43bdd5e4dfbcf49133119b52e00290ae3477
Your Name <jinathin@gmail.com>
2019-03-04 13:17:17 -0500

    fixing bugs and making turn and on and off

--- js/main.js
+++ js/main.js
@@ -17,9 +17,33 @@
 		// current times makes it reset so you can press the buttons fast as you want
 		audio.currentTime = 0;
 		audio.play();
+
+		// select the parent div and animate it 
+		// try slecting the matching audio element 
+		let key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+		key.classList.add('playing');
 	}
 
+	function removePlayingClass(e) {
+		// body...
+		//listen for a css transition to finish and then reset the element 
+		//by removing the playing class
+		// debugger;
+
+		if (e.propertyName == "transform") {
+			return;
+		} else {
+			console.log('do some stuff here, transition is done');
+			e.currentTarget.classList.remove('playing');
+		}
+	}
 	window.addEventListener('keydown', playDrumSound);
+
+	// grab all of the keyboard keys that have the .key class applied
+	const keys = Array.from(document.querySelectorAll('.key'));
+
+	// add a transition end to each key so that we can reset it
+	keys.forEach(key => key.addEventListener('transitionend', removePlayingClass));
 
 
 })();
